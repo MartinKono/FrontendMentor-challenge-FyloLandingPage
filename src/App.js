7c1db6c19d15c0db677291221de5e946e5891424
MartinKono <martin.konopasek@proton.me>
2023-11-07 12:17:12 -0500

    added intro section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss'
 import Navbar from './components/Navbar'
+import Intro from './components/Intro'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Intro/>
     </div>
   )
 }
