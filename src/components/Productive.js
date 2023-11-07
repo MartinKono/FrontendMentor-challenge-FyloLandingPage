4c665c1e8f83fc50a09d08077ec6417d30c026f4
MartinKono <martin.konopasek@proton.me>
2023-11-07 14:48:07 -0500

    added react icons and edited arrow icon in productive section

--- src/components/Productive.js
+++ src/components/Productive.js
@@ -1,6 +1,6 @@
 import './Productive.scss'
-import IconArrow from '../images/svgs/IconArrow'
 import illustration_productive from '../images/illustration-stay-productive.png'
+import { BsFillArrowRightCircleFill } from 'react-icons/bs'
 
 const Productive = () => {
   return (
@@ -10,7 +10,7 @@ const Productive = () => {
             <h1>Stay productive,<br/>wherever you are</h1>
             <p>Never let location be an issue when accessing your files. Fylo has you<br/>covered for all of your file storage needs.</p>
             <p>Securely share files and folders with friends, family and collegues for live<br/>collaboration. No email attachments required.</p>
-            <p id='link'><a href='#'>See how Fylo works</a><IconArrow/></p>
+            <p id='link'><a href='#'>See how Fylo works<BsFillArrowRightCircleFill/></a></p>
         </div>
     </section>
   )
