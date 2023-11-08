305b66caea5634001b2346a7b7cc7e03bba68420
MartinKono <martin.konopasek@proton.me>
2023-11-08 06:37:01 -0500

    added email checking feature

--- src/components/Access.js
+++ src/components/Access.js
@@ -2,17 +2,31 @@ import './Access.scss'
 import { useState } from 'react'
 
 const Access = () => {
+  
+  const [email, setEmail] = useState("")
+  const [error, setError] = useState(false)
 
-  const [emailError, setEmailError] = useState(true)
+  const formHandler = (event) => {
+    event.preventDefault()
+
+    const validRegex = /^^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    if (validRegex.test(email)) {
+        console.log('Sign up!!!')
+    } else {
+        setError(true)
+        setTimeout(() => setError(false), 2000)
+    }
+  }
 
   return (
     <div className='access'>
         <h1>Get early access today</h1>
         <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support tem would be happy to help you.</p>
-        <form className='access-form'>
+        <form className='access-form' onSubmit={formHandler}>
             <div className='email-container'>
-                <input type='text' placeholder='email@example.com'/>
-                {emailError ? <p className='error'>Please enter a valid email address</p> : null}
+                <input type='text' placeholder='email@example.com' onChange={(event)=>{setEmail(event.target.value)}} value={email || ''}/>
+                {error ? <p className='error'>Please enter a valid email address</p> : null}
             </div>
             <input type='submit' value='Get Started For Free'/>
         </form>
